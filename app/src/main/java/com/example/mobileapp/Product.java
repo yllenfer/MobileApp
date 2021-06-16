f72535e0f9110f28842393d9e66b3bcd5bef56e6
yllenfer <fer19020@byui.edu>
2021-06-15 20:43:28 -0500

    Purchase and background

--- app/src/main/java/com/example/mobileapp/Product.java
+++ app/src/main/java/com/example/mobileapp/Product.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class productsActivity extends AppCompatActivity {
+public class Product extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_products);
+        setContentView(R.layout.activity_product);
     }
 }
