021fdfa5d8ce565b670330e32b079662f2cd753c
yllenfer <fer19020@byui.edu>
2021-06-24 19:08:59 -0500

    going back function, again

--- app/src/main/java/com/example/mobileapp/cartActivity.java
+++ app/src/main/java/com/example/mobileapp/cartActivity.java
@@ -1,14 +1,40 @@
 package com.example.mobileapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
+import android.view.WindowManager;
 
 public class cartActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @SuppressWarnings("DEPRECATION")
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cart);
+        setContentView(R.layout.activity_log_in);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            final WindowInsetsController insetsController = getWindow().getInsetsController();
+            if (insetsController != null) {
+                insetsController.hide(WindowInsets.Type.statusBars());
+            }
+        } else {
+            getWindow().setFlags(
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN
+            );
+        }
     }
+
+    public void goToCheckout(View view) {
+        Intent intent  = new Intent(cartActivity.this, Checkout.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
