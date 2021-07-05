e5a8bcce29699dc1849c2441c6dc797c1e0a88f2
Yllen Fernandez <yllenfernandez@gmail.com>
2021-07-05 16:14:05 -0500

    Product activity working. Homefragment error.

--- app/src/main/java/com/example/mobileapp/firestore/FirebaseClass.java
+++ app/src/main/java/com/example/mobileapp/firestore/FirebaseClass.java
@@ -1,19 +1,15 @@
 package com.example.mobileapp.firestore;
 
 
-
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
 import com.example.mobileapp.AddProduct;
-import com.example.mobileapp.Checkout;
 import com.example.mobileapp.Product;
-import com.example.mobileapp.models.AddressModel;
 import com.example.mobileapp.models.ProductModel;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.example.mobileapp.utils.Constants;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -24,14 +20,15 @@ import com.google.firebase.database.ValueEventListener;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class FirebaseClass {
-    private static DatabaseReference db;
 
-    public static void registerProduct(AddProduct addProduct, ProductModel productInfo) {
+
+    public void registerProduct(AddProduct addProduct, ProductModel productInfo) {
+
+        DatabaseReference db;
+
         db = FirebaseDatabase.getInstance().getReference();
         db.addValueEventListener(new ValueEventListener() {
             @Override
@@ -47,36 +44,60 @@ public class FirebaseClass {
             }
         });
 
+
+//        db.collection("products")
+//                .document(productInfo.getProduct_name())
+//                .set(productInfo, SetOptions.merge())
+//                .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                    @Override
+//                    public void onSuccess(Void aVoid) {
+//                        Log.d(FirestoreClass.class.toString(), "Document added");
+//                        Toast.makeText(addProduct, "product added succesfully", Toast.LENGTH_LONG).show();
+//
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.w(FirestoreClass.class.toString(), "Error adding document", e);
+//                        Toast.makeText(addProduct, "process failed", Toast.LENGTH_LONG).show();
+//                    }
+//                });
     }
 
-    public static void addAddress(Checkout checkout, AddressModel address) {
-        db = FirebaseDatabase.getInstance().getReference().child("Users").child(address.getUser_id());
+       public void uploadProductDetails(AddProduct addProduct, Product product) {
 
-        Map<String, Object> addressInfo = new HashMap<>();
-        addressInfo.put("address", address.getAddress());
-        addressInfo.put("postalCode", address.getPostal_code());
-        addressInfo.put("state", address.getState());
 
-        db.updateChildren(addressInfo);
 
-    }
 
-    public void uploadProductDetails(AddProduct addProduct, Product product) {
+        }
 
-    }
 
-    public static String getUserID() {
-        String userID = "";
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//        public void getProductList(FirebaseClass firebaseClass){
+//
+//        DatabaseReference db = null;
+//        db.child(Constants.PRODUCTS)
+//                .equals(Constants.USER_ID, getCurrentID())
+//                .get();
+//                .addOnSuccessListener{ document -> Log.e("Product List", document.toString();
+//                ArrayList<String> Products = new ArrayList<String>();
+//
+//                for(String i : document.documents){
+//                    String product = i.toObject()(Product:: class.java)
+//                    product!!.Constants.product_id = i.id
+//
+//                            productList.append(product);
+//                }
 
-        assert user != null;
-        userID = user.getUid();
+//            }
 
-        return userID;
+        }
 
-    }
 
-}
+
+
+//}
+
 
 
 
